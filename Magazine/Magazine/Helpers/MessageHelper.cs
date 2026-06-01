f183eac120b012ac35b59d305aa06ceef289b82e
EuphoriaDLL <grancher500@gmail.com>
2026-06-01 23:54:29 +0300

    final

---
+++ Magazine/Magazine/Helpers/MessageHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Magazine.Helpers
+{
+    public class MessageHelper
+    {
+        public void ShowInfo(string message)
+        {
+            MessageBox.Show(message, "Informaation", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+        public void ShowError(string message)
+        {
+            MessageBox.Show(message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+        public void ShowWarning(string message)
+        {
+            MessageBox.Show(message, "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+        }
+    }
+}
---
+++ Magazine/Magazine/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Magazine.Helpers;
+using Magazine.Statics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Magazine
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Entities _db = new Entities();
+        private MessageHelper _mh = new MessageHelper();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LogInButton_Click(object sender, RoutedEventArgs e)
+        {
+            string login = LoginEnter.Text;
+            string password = PasswordEnter.Password;
+
+            var Users = _db.Users.Where(s => login == s.Login && s.Password == password).FirstOrDefault();
+            if (Users == null)
+            {
+                _mh.ShowError("Введен не верный логин или пароль!");
+                return;
+            }
+            else
+            {
+                CurrentSession.CurrentUser = Users;
+                new ProductWindow(Users).Show();
+                Close();
+            }
+        }
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            new ProductWindow().Show();
+            Close();
+        }
+    }
+}
